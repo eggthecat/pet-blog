22ac0d0c467aa8bf46c4fcc7d285c6bd6abb2b2e
Andrew Peterson and dominic montelongo <dominic@>
2019-08-13 10:36:57 -0700

    Add remove/add class for background in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
-$(document).ready(function(){
-  $("button#black").click(function(){
+$(document).ready(function() {
+  $("button#black").click(function() {
+    $("body").removeClass();
     $("body").addClass("black-background");
-    $("body").addColor("white-font");
-    $("body").css("background-color","white")
-  })
-  $("button#white").click(function(){
-    $("body").addClass("white-background");
-    $("body").addColor("black-font");
-    $("body").css("background-color","black")
-  })
+    // $("body").addColor("white-font");
+    // $("body").css("background-color","white")
+  });
+  $("button#cyan").click(function() {
+    $("body").removeClass();
+    $("body").addClass("cyan-background");
+    // $("body").addColor("black-font");
+    // $("body").css("background-color","black")
+  });
 });
