71376a6213cd6eb494d0be90eabde8d52732fa37
Andrew Peterson and dominic montelongo <andrew@>
2019-08-13 10:39:17 -0700

    Add font color to blk-bkgrnd in css

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,10 @@ $(document).ready(function() {
   $("button#black").click(function() {
     $("body").removeClass();
     $("body").addClass("black-background");
-    // $("body").addColor("white-font");
-    // $("body").css("background-color","white")
   });
+  
   $("button#cyan").click(function() {
     $("body").removeClass();
     $("body").addClass("cyan-background");
-    // $("body").addColor("black-font");
-    // $("body").css("background-color","black")
   });
 });
